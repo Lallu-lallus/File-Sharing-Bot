18c3cb7b79e9446ebcaed427ce36bf0a0e30b294
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-09-12 20:55:26 +0530

    Update support.py

--- database/support.py
+++ database/support.py
@@ -19,3 +19,34 @@ async def users_info(bot):
         else:
             blocked += 1
     return users, blocked
+Mmm bro I set but cannot working the cmd
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["status"]))
+
+async def _status(_, m: Message):
+
+    total, used, free = shutil.disk_usage(".")
+
+    total = humanbytes(total)
+
+    used = humanbytes(used)
+
+    free = humanbytes(free)
+
+    cpu_usage = psutil.cpu_percent()
+
+    ram_usage = psutil.virtual_memory().percent
+
+    disk_usage = psutil.disk_usage('/').percent
+
+    total_users = await db.total_users_count()
+
+    await m.reply_text(
+
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** {total_users}",
+
+        parse_mode="Markdown",
+
+        quote=True
+
+    )
